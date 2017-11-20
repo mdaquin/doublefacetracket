cd9f580905999ce1f84a0d490664f9371af50a83
Mathieu d'Aquin <mathieu@daquin.net>
2017-11-20 16:24:24 +0000

    Create trctrl.js

---
+++ trctrl.js
@@ -0,0 +1,39 @@
+// insert with 
+
+
+// ----- 
+
+var script = document.createElement('script');
+script.src = 'https://cdn.rawgit.com/auduno/clmtrackr/dev/build/clmtrackr.min.js';
+
+script.onload = function(){
+  var videoinput = document.getElementsByTagName("video")[0];
+  var ctracker = new clm.tracker();
+  ctracker.init();
+  ctracker.start(videoInput);
+  requestAnimationFrame(positionLoop);
+  positionLoop();
+}
+document.head.appendChild(script); //inject where you need it to be
+
+function positionLoop() {
+    var positions = ctracker.getCurrentPosition();
+    console.log(positions);
+    updatePosition(positions);
+    requestAnimationFrame(positionLoop);
+}
+
+var lastpos = false;
+
+function updatePositions(pos){
+  if (lastpos != pos) {
+    if (!pos){
+       console.log("No face detected")
+    } else {
+       console.log("face detected")
+       console.log(pos)   
+    }
+  }
+  lastpos = pos;
+    // should update the turn
+}
