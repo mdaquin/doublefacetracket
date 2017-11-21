9c79c05b0905c2d27ab014ae51e8f9488121be24
Mathieu d'Aquin <mathieu@daquin.net>
2017-11-21 09:52:48 +0000

    Update changeface.js

--- changeface.js
+++ changeface.js
@@ -1,1 +1,5 @@
 $("video:eq(1)").attr("src", "https://i.amz.mshcdn.com/PG0HsRQsD91FSWhz4iRyZU-lQt0=/fit-in/1200x9600/http%3A%2F%2Fmashable.com%2Fwp-content%2Fuploads%2F2013%2F08%2Fobama20.gif")
+// https://media.giphy.com/media/OJe7hsN3Mw5W/giphy.gif
+// https://thumbs.gfycat.com/GrotesqueDeepGrizzlybear-max-1mb.gif 
+// https://data.whicdn.com/images/55630517/original.gif
+// https://media.giphy.com/media/WwGF7Bkaqduta/giphy.gif
